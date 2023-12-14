64d7efe5d6093a4fb0fe073a770c09345e467836
Rahul Mahato <mahatorahul2901@gmail.com>
2023-12-14 19:28:30 +0530

    adding class.js

--- week-1/offline-class-1/level-2/01-Class.js
+++ week-1/offline-class-1/level-2/01-Class.js
@@ -1,13 +1,34 @@
 //Class and Object
 
+//A class is a blueprint
 class Animal {
   constructor(name, legCount) {
     this.name = name
     this.legCount = legCount
   }
+
+  //static method - When we know, things which will be fixed for every Animal class(then we use static method for it).
+  // this method is not associated with any object, it is associated with class. (it instanciate only once for the first call only)
+  static myType(){
+    console.log("Animal Type");
+  }
+  
+  //instance method
   describe() {
     return `${this.name} has ${this.legCount} legs`
   }
 }
+
+//object, creating real object of the blueprint
+let object1 = new Animal("Dog",4);
+let object2 = new Animal("Cat",4); //using same blueprint we can create different real world object, this the benifit of the concept classes and objects.
+
+let dog = object1.describe();
+console.log(dog);
+let cat = object2.describe();
+console.log(cat);
+
+//static method call
+console.log(Animal.myType());
 
 
