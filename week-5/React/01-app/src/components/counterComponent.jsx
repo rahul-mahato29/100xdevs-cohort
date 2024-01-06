0fa6b1dce0ea7a8f6196648c1babefc948dbd158
Rahul Mahato <mahatorahul2901@gmail.com>
2024-01-06 23:14:19 +0530

    update

--- week-5/React/01-app/src/components/counterComponent.jsx
+++ week-5/React/01-app/src/components/counterComponent.jsx
@@ -3,18 +3,19 @@ import React, {useState} from 'react';
 const CounterComponent = () => {  
     const [count, setCount] = useState(0);
 
-    function check(num){
-        if(num % 2 == 0){
-           return <span>Even🙂</span>;
-        }else{
-           return <span>Odd😐</span>;
-        }
-    }
+    // function check(num){
+    //     if(num % 2 == 0){
+    //        return <span>Even🙂</span>;
+    //     }else{
+    //        return <span>Odd😐</span>;
+    //     }
+    // }
 
     return (
         <div>
         <h3>Count : {count}</h3>
-        <h2>I am {check(count)}</h2>
+        {/* <h2>I am {check(count)}</h2> */}
+        <h2>I am {count % 2 == 0 ? 'Even' : 'Odd'}</h2>
         <button onClick={() => setCount(count+1)}>Increment</button>
         <button onClick={() => setCount(count-1)}>Decrement</button>
         </div>
