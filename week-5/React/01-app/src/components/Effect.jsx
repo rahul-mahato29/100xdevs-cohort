0cc2b2702959b773f4402e7aadc852ac579e194f
Rahul Mahato <mahatorahul2901@gmail.com>
2024-01-08 23:20:17 +0530

    update

--- week-5/React/01-app/src/components/Effect.jsx
+++ week-5/React/01-app/src/components/Effect.jsx
@@ -1,15 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect , useState} from 'react';
 
 const Effect = () => {
+
+    const [count, setCount] = useState(0);
 
     //useEffect
     useEffect( () => {
         console.log("mounting data")
-    }, []);
+    }, [count]);
 
     return (
-
-        <h3>Hi There, I came here to  help you in useEffect</h3>
+        <div>
+            <h3>Hi There, I came here to  help you in useEffect</h3>
+            <h3>Count : {count}</h3>
+            <button onClick={() => setCount(count+1)}>Increment</button>
+        </div>
     );
 }
 
