455f4ac9cb8172a642870679e70d47722a5ee817
Rahul Mahato <mahatorahul2901@gmail.com>
2024-04-20 22:49:38 +0530

    hook assignment

--- week-6/1-use-memo/src/components/Assignment1.jsx
+++ week-6/1-use-memo/src/components/Assignment1.jsx
@@ -1,22 +1,34 @@
+import { useMemo } from "react";
 import { useState } from "react";
 
 // In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input. 
 // Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
 
 export function Assignment1() {
-    const [input, setInput] = useState(0);
+    const [input, setInput] = useState(1);
+    const [test, setTest] = useState(false);
+
     // Your solution starts here
-    const expensiveValue = 0; 
-    // Your solution ends here
+    const expensiveValue = useMemo(() => {
+        console.log("Only recomputed when the input changes");
+        let result = 1;
+        for (let i = 1; i <= input; i++) {
+            result = result * i;
+        }
+        return result;
+    }, [input]);  //here this input is indicating, useMemo will only compute when there is any change in the input field
 
     return (
         <div>
-            <input 
-                type="number" 
-                value={input} 
-                onChange={(e) => setInput(Number(e.target.value))} 
+            <input
+                type="number"
+                value={input}
+                onChange={(e) => setInput(Number(e.target.value))}
             />
             <p>Calculated Value: {expensiveValue}</p>
+            {/* The change in the below "TEST" state will not trigger the re-rendering of expensiveValue, because we are using useMemo */}
+            <button onClick={() => setTest(!test)}>Click me the change the another state</button> 
+            <p>{test ? "successful" : "failed"}</p>
         </div>
     );
 }
--- week-6/1-use-memo/src/components/Assignment2.jsx
+++ week-6/1-use-memo/src/components/Assignment2.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 // In this assignment, you will create a component that renders a large list of sentences and includes an input field for filtering these items. 
 // The goal is to use useMemo to optimize the filtering process, ensuring the list is only re-calculated when necessary (e.g., when the filter criteria changes).
@@ -20,7 +20,14 @@ export function Assignment2() {
     const [sentences, setSentences] = useState(ALL_WORDS);
     const [filter, setFilter] = useState("");
 
-    const filteredSentences = sentences.filter(x => x.includes(filter))
+    //here, it is rendering a large list of sentences, so this is an expensive operation.
+    // const filteredSentences = sentences.filter(x => x.includes(filter))  //passing more than one value in the dependency array
+
+    //right way to handle an expensive operation, using useMemo
+    const filteredSentences = useMemo(() => {
+        return sentences.filter(x => x.includes(filter))
+    }, [filter])
+
 
     return <div>
         <input type="text" onChange={(e) => {
