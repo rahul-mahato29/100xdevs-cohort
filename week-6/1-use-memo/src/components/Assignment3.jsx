13eb3b05003a21f8f72ddf62131eacef414edfea
Rahul Mahato <mahatorahul2901@gmail.com>
2024-04-21 09:47:45 +0530

    useMemo-Done

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -8,12 +8,28 @@ export const Assignment3 = () => {
         { name: 'Chips', value: 20 },
         { name: 'Onion', value: 30 },
         { name: 'Tomato', value: 30 },
+        { name: 'Ice-Cream', value: 80 },
+        { name: 'Cold-Drinks', value: 120 }
         // Add more items as needed
     ]);
 
     // Your code starts here
-    const totalValue = 0;
-    // Your code ends here
+    const totalValue = useMemo(() => {
+        let total = 0;
+        items.map(info => {
+            total = total + info.value;
+        })
+        console.log("hi");
+        return total;
+    }, [items])
+    // In the above code, the useMemo is invoked only during the initial render for the first time, 
+    // because in our case we are not invoking the setItem to change the state.
+
+// IMP :
+// 1. useMemo is invoked both during the initial render(frist time rendeing of whole component) and when its dependencies change.
+// 2. During the initial render, useMemo calculates the initial memoized value of totalValue.
+// 3. On subsequent renders, useMemo will recalculate totalValue if the items array changes.
+
     return (
         <div>
             <ul>
@@ -21,7 +37,7 @@ export const Assignment3 = () => {
                     <li key={index}>{item.name} - Price: ${item.value}</li>
                 ))}
             </ul>
-            <p>Total Value: {totalValue}</p>
+            <p>Total Value : ${totalValue}</p>
         </div>
     );
 };
